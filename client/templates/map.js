4cfce4e472d503d3265fd9aa3553f04b230ec670
Johan Reimann <johre099@student.liu.se>
2016-02-22 16:57:58 +0100

    Visualize taxis with red dots on map

--- client/templates/map.js
+++ client/templates/map.js
@@ -1,6 +1,11 @@
 var theMap = new GMap();
+var dsv = d3.dsv(";", "text/plain");
 
 Template.map.onRendered(function createMap() {
   var self = this;
   theMap.setup(self.firstNode);
+  dsv('/small-data.dsv', function (error, data) {
+    if (error) throw error;
+    theMap.addData(data);
+  });
 });
