3a4a4dc4779d8568a90aef1ac8fd171e0412506e
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-24 09:23:22 +0100

    Replace tabs with spaces in map-libs.js

--- client/libs/map-lib.js
+++ client/libs/map-lib.js
@@ -13,32 +13,32 @@ GMap.prototype.setup = function(node) {
 GMap.prototype.addData = function(data) {
   var overlay = new google.maps.OverlayView();
   overlay.onAdd = function() {
-  	var layer = d3.select(this.getPanes().overlayLayer).append("div")
-  		.attr("class", "taxis");
+    var layer = d3.select(this.getPanes().overlayLayer).append("div")
+      .attr("class", "taxis");
 
-  	overlay.draw = function() {
-  		var projection = this.getProjection(),
-  			padding = 10;
-  		var marker = layer.selectAll("svg")
-  			.data(d3.entries(data))
-  			.each(transform) // update existing markers
+    overlay.draw = function() {
+      var projection = this.getProjection(),
+        padding = 10;
+      var marker = layer.selectAll("svg")
+        .data(d3.entries(data))
+        .each(transform) // update existing markers
         .enter().append("svg")
-  			.each(transform)
-  			.attr("class", "marker");
+        .each(transform)
+        .attr("class", "marker");
 
-  		marker.append("circle")
-  		.attr("r", 4.0)
-  		.attr("cx", padding)
-  		.attr("cy", padding);
+      marker.append("circle")
+      .attr("r", 4.0)
+      .attr("cx", padding)
+      .attr("cy", padding);
 
-  		function transform(d) {
+      function transform(d) {
         d = new google.maps.LatLng(parseFloat(d.value.y_coord), parseFloat(d.value.x_coord));
         d = projection.fromLatLngToDivPixel(d);
         return d3.select(this)
             .style("left", (d.x - padding) + "px")
             .style("top", (d.y - padding) + "px");
       };
-  	};
+    };
   };
 
   overlay.setMap(this.map);
