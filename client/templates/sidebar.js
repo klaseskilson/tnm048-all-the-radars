49dce798810479fc7118bace682f5d06fe144dc5
Klas Eskilson <klas.eskilson@gmail.com>
2016-02-29 16:55:07 +0100

    Add sidebar and SCSS

---
+++ client/templates/sidebar.js
@@ -0,0 +1,22 @@
+Template.sidebar.helpers({
+  hideSidebar(value) {
+    value = value || true;
+    let dataContext = Session.get('dataContext');
+    let subscription = dataContext && dataContext.subscription;
+    return subscription !== 'getTaxisById' ? value : false;
+  }
+});
+
+Template.sidebar.events({
+  'click .js-exit-taxi': () => {
+    let dataContext = Session.get('dataContext');
+    dataContext.subscription = 'getTaxisByDate';
+    Session.set('dataContext', dataContext);
+  }
+});
+
+Template.calendar.helpers({
+  weeks() {
+    return [1, 2, 3, 4, 5];
+  }
+});
