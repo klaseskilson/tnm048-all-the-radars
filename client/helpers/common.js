09eb33ef0943c1d132bd50758e019dc4a6894243
Klas Eskilson <klas.eskilson@gmail.com>
2016-03-08 18:43:18 +0100

    Add spinning loading indicator to map and cluster

--- client/helpers/common.js
+++ client/helpers/common.js
@@ -1,2 +1,3 @@
 Template.registerHelper('session', key => Session.get(key));
 Template.registerHelper('debugger', () => { debugger });
+Template.registerHelper('icon', icon => Spacebars.SafeString(`<span class="fa fa-${icon}"></span>`));
