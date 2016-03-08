b9d6074bb10059a56ed542f85e1532bcf4f2aadd
Klas Eskilson <klas.eskilson@gmail.com>
2016-03-08 13:30:01 +0100

    Add clustering form and placeholder function

--- client/templates/clusterbar.js
+++ client/templates/clusterbar.js
@@ -6,7 +6,7 @@ Template.clusterbar.helpers({
   }
 });
 
-Template.clusterbar.onCreated(function () {
+Template.clusterForm.onCreated(function () {
   var template = this;
 
   template.autorun(() => {
@@ -15,3 +15,49 @@ Template.clusterbar.onCreated(function () {
 
 	});
 });
+
+Template.clusterForm.onCreated(function () {
+  this.clustering = new ReactiveVar(false);
+});
+
+Template.clusterForm.helpers({
+  loading () {
+    return Template.instance().clustering.get();
+  },
+});
+
+Template.clusterForm.events({
+  'submit .js-cluster-form' (event) {
+    event.preventDefault();
+    const cars = parseInt(event.currentTarget.cars.value);
+    const radius = parseFloat(event.currentTarget.radius.value);
+
+    const clustering = Template.instance().clustering;
+    clustering.set(true);
+
+    cluster(cars, radius).then((data) => {
+      console.log('DONE!', data);
+      clustering.set(false);
+    }, (error) => {
+      console.log('ERROR!', error);
+      clustering.set(false);
+    });
+  }
+});
+
+function cluster (cars, radius) {
+  return new Promise((resolve, reject) => {
+    //
+    // cluster here!
+    //
+
+    if (cars > 10) {
+      // simulate loading with setTimeout
+      setTimeout(() => {
+        resolve(cars);
+      }, 3000);
+    } else {
+      reject(cars);
+    }
+  });
+}
