dd9f770fa30a7950b77c0b4fac5931c1ee00d489
Klas Eskilson <klas.eskilson@gmail.com>
2016-03-14 20:52:10 +0100

    Add time to taxi list under cluster

--- client/templates/clusterList.js
+++ client/templates/clusterList.js
@@ -21,7 +21,7 @@ Template.taxiList.onCreated(function () {
   const geocoder = new google.maps.Geocoder();
 
   const getPosition = ({ lat, lng }) => {
-    return new Promise((resolve, reject = console.warn) => {
+    return new Promise((resolve, reject = console.warn.bind(console)) => {
       const latLng = new google.maps.LatLng(lat, lng);
 
       geocoder.geocode({ latLng }, (results, status) => {
@@ -36,16 +36,22 @@ Template.taxiList.onCreated(function () {
     });
   };
 
-  const { taxis } = Template.currentData();
-  taxis.map(taxi => {
-    taxi.positionName = new ReactiveVar('');
-    const position = {
-      lat: taxi.y_coord,
-      lng: taxi.x_coord,
-    };
-    getPosition(position).then(address => taxi.positionName.set(address));
-    return taxi;
-  })
+  const data = Template.currentData();
+  data.taxis = _.chain(data.taxis)
+    .sortBy(taxi => {
+      console.log(taxi.date.getTime())
+      return taxi.date.getTime()
+    })
+    .map(taxi => {
+      taxi.positionName = new ReactiveVar('');
+      const position = {
+        lat: taxi.y_coord,
+        lng: taxi.x_coord,
+      };
+      getPosition(position).then(address => taxi.positionName.set(address));
+      return taxi;
+    })
+    .value();
 });
 
 Template.taxiList.helpers({
@@ -56,6 +62,10 @@ Template.taxiList.helpers({
 
   position () {
     return this.positionName.get();
+  },
+
+  time () {
+    return moment(this.date).format('HH:mm');
   },
 });
 
